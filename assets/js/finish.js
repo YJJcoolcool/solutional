59e0398371b7d77114c4dee3da509bf1d760b194
Yam Jun Jie <yjjcoolcool@gmail.com>
2022-01-04 23:03:23 +0800

    Finish section after completing the quiz

---
+++ assets/js/finish.js
@@ -0,0 +1,25 @@
+var getparams = new URLSearchParams(location.search.substring(1));
+var uid = getparams.get("uid");
+var score = getparams.get("score");
+var numqn = getparams.get("numqn");
+
+$(document).ready(function() 
+{
+    $.ajax({
+        type: "GET",
+        url: "/solutional/data/modules.json",
+        success: function (response) {
+            process(response);
+        },
+        error: function (obj, textStatus, errorThrown) {
+            console.log("Error "+textStatus+": "+errorThrown);
+        }
+    });
+});
+
+function process(data){
+    console.log(data)
+    var modulename = Object.keys(data).find(key => data[key]['uid'] === uid);
+    document.getElementById('module').innerHTML = "Module: "+modulename;
+    document.getElementById('score').innerHTML = "Score: "+score+"/"+numqn;
+}

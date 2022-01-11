4e1c113ae250e92a1fc97a0453c2da59a8e005ac
Yam Jun Jie <yjjcoolcool@gmail.com>
2022-01-11 21:53:43 +0800

    Added multi-select questions

--- assets/js/startquiz.js
+++ assets/js/startquiz.js
@@ -8,7 +8,8 @@ var qndistribution = []
 var currentqn = -1;
 var correctqns = 0;
 var qnlist = [];
-var answers;
+var options;
+var enteredanswer = []; // Only used for non-type 1 questions
 var shortforms = ["wotf","Which of the following"]
 //var audio = new Audio('/solutional/assets/danger.mp3');
 
@@ -36,8 +37,8 @@ function process(data){
     for (var i=0; i < numsec; i++){
         qndistribution.push(Math.floor(numqn/numsec))
         if (remainderqns>0){
-            qndistribution[qndistribution.length-1]+=1
-            remainderqns-=1
+            qndistribution[qndistribution.length-1]++;
+            remainderqns--;
         }
     }
     console.log(qndistribution)
@@ -52,7 +53,7 @@ function process(data){
                 }
                 qnlist.push(qn)
             }
-            j+=1;
+            j++;
         }
     }
     document.getElementById('progress').max=numqn;
@@ -63,9 +64,12 @@ function process(data){
 }
 
 function nextQn(){
-    currentqn+=1
+    currentqn++
     document.querySelectorAll('.clone').forEach(e => e.remove());
+    document.getElementById('additionalnotes').innerHTML="";
     //document.getElementById('socialcredit').style.display = 'none';
+
+    // Disable next question button
     document.getElementById('nextqn').disabled=true;
     document.getElementById('nextqn').innerHTML='Waiting for answer...';
     document.getElementById('qnnum').innerHTML='Question '+(currentqn+1)+" of "+numqn;
@@ -74,50 +78,128 @@ function nextQn(){
         question = question.replace("@`"+shortforms[i*2],shortforms[i*2+1])
     }
     document.querySelector("#question").innerHTML = question;
-    answers = qnlist[currentqn]['answers'];
-    // Randomise Answers
-    for (var i = answers.length - 1; i > 0; i--) {
+    options = qnlist[currentqn]['options'];
+    // Randomise options
+    for (var i = options.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-        var temp = answers[i];
-        answers[i] = answers[j];
-        answers[j] = temp;
+        var temp = options[i];
+        options[i] = options[j];
+        options[j] = temp;
     }
-    var answerbuttontemplate = document.querySelector('#answerbuttontemplate');
-    var answerlist = document.getElementById('buttonarea');
-    var count = 0;
-    answers.forEach((element)=>{
-        element = element.replace('[]','')
-        var clone = answerbuttontemplate.content.cloneNode(true);
-        clone.querySelector("button").innerText=element;
-        clone.querySelector("button").id=count;
-        clone.querySelector("div").classList.add('clone');
-        answerlist.appendChild(clone)
-        count+=1;
-    })
+
+    // Different types of questions
+    /*
+    1 - Single Choice Question (MCQ)
+    2 - Multiple Choice Question (MCQ)
+    */
+    var type = qnlist[currentqn]['type']
+    if (type===1 || type===2) {
+        // Single choice template
+        var answerbuttontemplate
+        (type===1) ? answerbuttontemplate = document.querySelector('#answerbuttontemplate') : answerbuttontemplate = document.querySelector('#multianswerbuttontemplate');
+        var answerlist = document.getElementById('buttonarea');
+        var count = 0;
+        // Create a button for each option, replace the correct ans brackets [] with empty string
+        options.forEach((element)=>{
+            element = element.replace('[]','')
+            var clone = answerbuttontemplate.content.cloneNode(true);
+            clone.querySelector("button").innerText=element;
+            clone.querySelector("button").id=count;
+            clone.querySelector("div").classList.add('clone');
+            answerlist.appendChild(clone)
+            count++;
+        })
+        if (type===2) {
+            document.getElementById('nextqn').onclick = function() {multiCheckAnswer()};
+            document.getElementById('additionalnotes').innerHTML="You may select 1 or more answers.";
+        }
+    }
 }
 
+// For single select
 function checkAnswer(id){
-    if (document.getElementById('nextqn').disabled){
-        if (answers[id].startsWith('[]')){
-            document.getElementById(id).classList.remove('btn-dark')
-            document.getElementById(id).classList.add('btn-success')
-            correctqns+=1
-            document.getElementById('correctqns').innerHTML = correctqns+"/"+numqn+" Questions Correct";
-            document.getElementById('score').value=correctqns;
+    if (document.getElementById('nextqn').disabled) {
+        // Check if answer is correct
+        if (options[id].startsWith('[]')) {
+            document.getElementById(id).classList.remove('btn-dark');
+            document.getElementById(id).classList.add('btn-success');
+            correctqns++;
         } else {
-            document.getElementById(id).classList.remove('btn-dark')
-            document.getElementById(id).classList.add('btn-danger')
+            document.getElementById(id).classList.remove('btn-dark');
+            document.getElementById(id).classList.add('btn-danger');
             /*document.getElementById('socialcredit').style.display = 'block';
             setTimeout(()=>{document.getElementById('socialcredit').style.display = 'none';},5500)
             audio.play();*/
         }
-        document.getElementById('progress').value=currentqn+1;
-        if (currentqn>=numqn-1) {
-            document.getElementById('nextqn').style.display = 'none';
-            document.getElementById('submit').style.display = 'block';
-        } else {
-            document.getElementById('nextqn').innerHTML='Continue to next question';
-        }
+        finishQn();
+    }
+}
+
+function finishQn(){
+    document.getElementById('correctqns').innerHTML = correctqns+"/"+numqn+" Questions Correct";
+    document.getElementById('score').value=correctqns;
+    // Increase progress bar
+    document.getElementById('progress').value=currentqn+1;
+    // Check if last question
+    if (currentqn>=numqn-1) {
+        document.getElementById('nextqn').style.display = 'none';
+        document.getElementById('submit').style.display = 'block';
+    } else {
+        document.getElementById('nextqn').onclick = function() {nextQn()};
+        document.getElementById('nextqn').innerHTML='Continue to next question';
         document.getElementById('nextqn').disabled=false;
+    }
+    // Clear selected options
+    enteredanswer = [];
+}
+
+// For multi select
+function selectAnswer(id){
+    if (document.getElementById('nextqn').disabled || document.getElementById('nextqn').innerText==="Submit Answers") {
+        if (enteredanswer.includes(id)){
+            enteredanswer.splice(enteredanswer.indexOf(id),1)
+            document.getElementById(id).classList.remove('btn-info');
+            document.getElementById(id).classList.add('btn-dark');
+        } else {
+            enteredanswer.push(id)
+            document.getElementById(id).classList.remove('btn-dark');
+            document.getElementById(id).classList.add('btn-info');
+        }
+        if (enteredanswer.length>0){
+            document.getElementById('nextqn').innerHTML='Submit Answers';
+            document.getElementById('nextqn').disabled=false;
+        } else {
+            document.getElementById('nextqn').innerHTML='Waiting for answer...';
+            document.getElementById('nextqn').disabled=true;
+        }
+    }
+}
+
+function multiCheckAnswer(){
+    var correctanswers=[0,0,false];
+    for (var i=0; i<options.length; i++) {
+        if (enteredanswer.includes(i.toString())){
+            if (options[i].startsWith('[]')) {
+                correctanswers[0]++;
+                correctanswers[1]++;
+                document.getElementById(i).classList.remove('btn-info');
+                document.getElementById(i).classList.add('btn-success');
+            } else {
+                correctanswers[2]=true;
+                document.getElementById(i).classList.remove("btn-info");
+                document.getElementById(i).classList.add('btn-danger');
+            }  
+        } else if (options[i].startsWith('[]')) {
+            correctanswers[1]++;
+        }
+    }
+    console.log(correctanswers)
+    console.log(correctanswers[0]===correctanswers[1])
+    if (correctanswers[0]===correctanswers[1] && !correctanswers[2]){
+        correctqns++;
+    }
+    finishQn();
+    if (!(correctanswers[0]===correctanswers[1] && correctanswers[2])){
+        document.getElementById('nextqn').innerHTML+=' ('+correctanswers[0]+'/'+correctanswers[1]+' answers correct)'; 
     }
 }
